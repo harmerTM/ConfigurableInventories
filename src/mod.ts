66af34891fc9328093cf67147737a179041f6d13
Harmer <38035213+harmerTM@users.noreply.github.com>
2024-01-28 20:00:19 +0000

    Added cases & rigs. Readability+ in config.

--- src/mod.ts
+++ src/mod.ts
@@ -12,33 +12,30 @@ class ConfigurableInventories implements IPostDBLoadMod {
     const tables = databaseServer.getTables();
     const items = tables.templates.items;
 
-    const multiGridBackpacks = [
-      "5f5e46b96bdad616ad46d613", // Eberlestock F4 Terminator load bearing backpack Tiger Stripe
-      "5c0e805e86f774683f3dd637", // 3V Gear Paratus 3-Day Operators Tactical backpack
-      "6034d2d697633951dc245ea6", // Eberlestock G2 Gunslinger II backpack Dry Earth
-      "5d5d940f86f7742797262046", //Oakley Mechanism heavy duty backpack Black
-    ];
-
     let s = 0;
 
     for (const item of inventory) {
-      items[item.itemID]._props.GridLayoutName = "";
+      const { _props } = items[item.itemID];
+
+      _props.GridLayoutName = _props.RigLayoutName = item.layout;
 
       for (let g = 0; g < item.grid.length; g++, s++) {
-        const gridProps = items[item.itemID]._props.Grids[s]._props;
+        const gridProps = _props.Grids[s]._props;
+
         Object.assign(gridProps, {
           cellsH: item.grid[g].horizontal,
           cellsV: item.grid[g].vertical,
+          filters: item.removeFilters ? [] : gridProps.filters,
         });
-
-        if (item.removeFilters) {
-          gridProps.filters = [];
-        }
       }
+
+      /* i need to figure out a way to split it because of rigs!
+      if (item.layout == "") {
+        _props.Grids.splice(1)
+      }
+      */
       s = 0;
     }
-
-    multiGridBackpacks.forEach((id) => items[id]._props.Grids.splice(1));
   }
 }
 
