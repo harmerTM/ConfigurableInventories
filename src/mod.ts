4435806cd73d5f650d274bc2e371470fdbbb4757
Harmer <38035213+harmerTM@users.noreply.github.com>
2024-01-29 04:45:52 +0000

    Plate Carriers and new config.jsons
    Converted the config.ts into multiple config.jsonc per inventory (Backpack, Cases)
    Added plate carriers!
    Made sure to actually change the version number in the package.json...

--- src/mod.ts
+++ src/mod.ts
@@ -1,42 +1,75 @@
 import { DependencyContainer } from "tsyringe";
-
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
+import { VFS } from "@spt-aki/utils/VFS";
+import { jsonc } from "jsonc";
+import path from "path";
 
-//Pull in config
-import { inventory } from "../config/config.ts";
+class ConfigurableInventories implements IPostDBLoadMod 
+{
+    public postDBLoad(container: DependencyContainer): void 
+    {
+        const vfs = container.resolve<VFS>("VFS");
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const tables: IDatabaseTables = databaseServer.getTables();
+        const items = Object.values(tables.templates.items);
 
-class ConfigurableInventories implements IPostDBLoadMod {
-  public postDBLoad(container: DependencyContainer): void {
-    const databaseServer = container.resolve("DatabaseServer");
-    const tables = databaseServer.getTables();
-    const items = tables.templates.items;
+        const loadJsoncFile = (filename: string) =>
+            jsonc.parse(
+                vfs.readFile(path.resolve(__dirname, `../config/${filename}.jsonc`))
+            );
 
-    let s = 0;
+        // Load configurations for different item categories
+        const [backpacks, cases, plateCarriers, pockets, rigs, secureContainers] = [
+            "backpacks",
+            "cases",
+            "plateCarriers",
+            "pockets",
+            "rigs",
+            "secureContainers"
+        ].map(loadJsoncFile);
 
-    for (const item of inventory) {
-      const { _props } = items[item.itemID];
+        // Process and update item properties based on configurations
+        const processItems = (itemsList: any[]) => 
+        {
+            itemsList.forEach((item) => 
+            {
+                const { _props } = items.find((i) => i._id === item.itemID) || {};
+                if (_props) 
+                {
+                    _props.GridLayoutName = _props.RigLayoutName = item.layout;
 
-      _props.GridLayoutName = _props.RigLayoutName = item.layout;
+                    // Update grid properties
+                    item.grid.forEach(({ horizontal, vertical }, g) => 
+                    {
+                        const gridProps = _props.Grids[g]._props;
+                        Object.assign(gridProps, {
+                            cellsH: horizontal,
+                            cellsV: vertical,
+                            filters: item.removeFilters ? [] : gridProps.filters
+                        });
+                    });
 
-      for (let g = 0; g < item.grid.length; g++, s++) {
-        const gridProps = _props.Grids[s]._props;
+                    // Remove grids if layout is empty (for backpacks)
+                    if (item.layout === "" && itemsList === backpacks) 
+                    {
+                        _props.Grids.splice(1);
+                    }
+                }
+            });
+        };
 
-        Object.assign(gridProps, {
-          cellsH: item.grid[g].horizontal,
-          cellsV: item.grid[g].vertical,
-          filters: item.removeFilters ? [] : gridProps.filters,
-        });
-      }
-
-      /* i need to figure out a way to split it because of rigs!
-      if (item.layout == "") {
-        _props.Grids.splice(1)
-      }
-      */
-      s = 0;
+        // Process items for each category
+        processItems([
+            ...backpacks,
+            ...cases,
+            ...plateCarriers,
+            ...pockets,
+            ...rigs,
+            ...secureContainers
+        ]);
     }
-  }
 }
 
 module.exports = { mod: new ConfigurableInventories() };
